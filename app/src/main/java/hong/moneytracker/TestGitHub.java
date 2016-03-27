3acde8d19a9069530cbd551b829342d78d846e72
jackhong6 <jackhong6@gmail.com>
2016-03-27 15:48:55 -0700

    Test github commit from Jack.

---
+++ app/src/main/java/hong/moneytracker/TestGitHub.java
@@ -0,0 +1,10 @@
+package hong.moneytracker;
+
+/**
+ * Created by jackhong on 2016-03-27.
+ */
+public class TestGitHub {
+    public void JackHong() {
+        System.out.println("Jack Is awesome!!");
+    }
+}
