8d955a8df08fb83d843067783bf71ca72cb09025
jackhong6 <jackhong6@gmail.com>
2016-03-27 23:09:25 -0700

    Set fields.

--- app/src/main/java/hong/moneytracker/Friend.java
+++ app/src/main/java/hong/moneytracker/Friend.java
@@ -6,6 +6,7 @@ import java.util.List;
  * Created by jackhong on 2016-03-27.
  */
 public class Friend {
+    String name;
     List<Transaction> transactions;
 
     public Friend () {
--- app/src/main/java/hong/moneytracker/Transaction.java
+++ app/src/main/java/hong/moneytracker/Transaction.java
@@ -1,7 +1,15 @@
 package hong.moneytracker;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by jackhong on 2016-03-27.
  */
 public class Transaction {
+    private Date date;
+    private String description;
+    private List<Friend> friends;
+    private Float amount;
+
 }
